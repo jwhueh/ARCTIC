3a76823339956945124dc2030a1d80a136cb06ff
root <root@ts7250-4880e2.(none)>
2015-06-30 02:55:36 +0000

    doughc29

---
+++ Metrology/temp.py
@@ -0,0 +1,93 @@
+#! /usr/bin/python
+
+import serial
+import time
+
+class Metrology(object):
+	def __init__(self):
+		pass
+		self.ser = None
+		self.serial = '/dev/ttyS0'
+		self.sensors=[]
+		self.delay = 0.2
+		self.start()
+		self.findTempSensors()
+		self.setupSensors()
+
+	def run(self):
+		while True:
+			fout = open('temp.log','a')
+			output=time.strftime("%Y%m%dT%H%M%S")
+			for s in self.sensors:
+				output = output+','	
+				temp = self.readTemp(s)
+				output = output+str(temp)
+			fout.write(output+'\n')
+			fout.close()
+			
+			time.sleep(10)	
+
+
+	def setupSensors(self):
+		for s in self.sensors:
+			self.setResolution(s)
+			self.readTemp(s)
+		return
+			
+
+	def findTempSensors(self):
+		self.sensors.append(self.serWrite('S\r').rstrip('\r'))
+		while True:
+			out = self.serWrite('s\r')
+			if len(out) <=3:
+                                break
+			self.sensors.append(out.rstrip('\r'))
+		print self.sensors
+		return
+
+
+	def setResolution(self,dev):
+		self.deviceSelect(dev)
+		self.serWrite('W044E4B467F')
+		self.serWrite('R')
+
+	def start(self):
+		self.ser = serial.Serial(self.serial,9600, timeout=.1)
+		return
+
+	def readTemp(self, dev):
+		self.deviceSelect(dev)
+		self.serWrite('M')
+		self.serWrite('W0144')
+		time.sleep(10)
+		self.serWrite('M')
+		output = self.serWrite('W0ABEFFFFFFFFFFFFFFFFFF')
+		t = self.convert(output)	
+		return t
+
+	def deviceSelect(self, dev):
+		cmd = 'A%s\r' % str(dev)
+		self.ser.write(cmd)
+		time.sleep(self.delay)
+		self.ser.readline()
+		return
+
+	def serWrite(self, text1 = None, text2 = None):
+		if text2 == None:
+			cmd = '%s\r' % text1
+		self.ser.write(cmd)
+		time.sleep(self.delay)
+		out = self.ser.readline()
+		return out
+
+	def convert(self, sig):
+		lsb = sig[2:4]
+		msb = sig[4:6]
+		lm = msb+lsb
+		temp = int('0x%s' % lm,0)/16.
+		return temp
+
+if __name__ == "__main__":
+	m = Metrology()
+	#m.readTemp('1C00000365971D28')
+	m.run()
---
+++ experimental/shutter.py
@@ -0,0 +1,181 @@
+#! /usr/bin/python
+
+"""
+shutter.py
+
+This program connects to the pc104 shutter_interface c file and does the high level commanding and computation of the shutter
+"""
+
+__author__ = ["Caitlin Doughty", "Joseph Huehnerhoff"]
+__copyright__ = "NA"
+__credits__ = [""]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "NA"
+__email__ = "NA"
+__status__ = "Development"
+
+from multiprocessing import Process
+import time
+from time import gmtime, strftime
+from ctypes import *
+import thread
+
+class shutterControl(object):
+    def __init__(self):
+	"""
+	pin_l refers to the pin controlling the left shutter, pin_r the right
+	when the airhoses are located to the right of the apparatus	
+	"""
+        
+	self.shutter = CDLL("./shutter_interface.so")
+	self.waiting = CDLL("./shutter_watch.so")
+        self.pin_r = 76
+	self.pin_l = 77
+	self.delay = 0.5
+	self.home = None
+	self.open = None
+	self.right = None	# directional variable, next motion is right when true
+	self.running = True
+	print "opening connection"
+        self.shutter.openConnection()
+	time.sleep(.1)  #I tend to sleep after opening any connection because it may have some time dependent routines it needs feedback from
+	#self.shutter.moveShutter(61,1)  #needed for initialization of PC104
+
+    def end(self):
+	while True:
+	    exit = raw_input("Exit?")
+	    print exit
+	    if not exit:
+		self.waiting.stopLoop()
+	        self.running = False
+		break
+
+    def exerciseRoutine(self):
+	print "Just exercising.\n"
+	self.shutter.moveShutter(self.pin_l,0)
+        time.sleep(self.delay)
+        self.shutter.moveShutter(self.pin_r,0)
+        time.sleep(self.delay)
+        self.shutter.moveShutter(self.pin_r,1)
+        time.sleep(self.delay)
+        self.shutter.moveShutter(self.pin_l,1)
+        time.sleep(self.delay)
+        self.shutter.moveShutter(self.pin_l,0)
+        time.sleep(self.delay)
+        self.shutter.moveShutter(self.pin_r,0)
+        time.sleep(self.delay)
+	return
+
+    def sendHome(self):
+	""" Moves shutter to forward home position
+	Args:
+		None
+	Returns:
+		True if task completed
+	"""
+
+        self.shutter.moveShutter(self.pin_l,0)
+        time.sleep(self.delay)
+        self.shutter.moveShutter(self.pin_r,0)
+        self.checkStatus()
+	return
+
+    def checkStatus(self):
+	""" Checks the positional status of the shutter blades
+	Args:
+		None
+	Returns:
+		True if on track 1, False if on track 2.
+	"""
+	time.sleep(self.delay)
+	hallArray = self.hallArrayMake()
+	if hallArray[3][1] == hallArray[4][1]:
+	    if hallArray[3][1] == 0:
+		print 'Closed at home.\n'
+		self.home = True
+		self.open = False
+		self.right = True
+	    else:
+		print 'Closed.\n'
+		self.home = False
+		self.open = False
+		self.right = False
+	else: 
+	    print 'Exposing.\n'
+	    self.home = False
+	    self.open = True
+	return
+
+    def changeSense(self):
+	'''
+	calls shutter_watch methods to observe for changes in pin values, returns value pins changed to, initiates opening and closing of the shutters depending on position and pin value, does nothing if already in requested state
+	currently, after any motion will ask user if they want to continue
+	Args:
+		None
+	Returns:
+		None
+	'''
+	try:
+	    while self.running == True:
+		self.waiting.loop()
+	        if self.home == True:
+		    self.toPosRight(self.pin_r)
+		    self.waiting.loop()
+		    self.toPosRight(self.pin_l)
+		else:
+		    self.toPosLeft(self.pin_l)
+		    self.waiting.loop()
+		    self.toPosLeft(self.pin_r)
+		self.checkStatus()
+	except KeyboardInterrupt:
+		pass
+	return
+
+
+    def toPosRight(self,shutter):
+	'''
+	moves a single shutter to the reverse home position
+	Args:
+		shutter pin identifier
+	Returns:
+		None
+	'''
+
+	self.shutter.moveShutter(shutter,1)
+        return
+    
+
+    def toPosLeft(self,shutter):
+	'''
+	moves a single shutter to the home position
+	Args:
+                shutter pin identifier
+        Returns:
+                None
+        '''
+
+	self.shutter.moveShutter(shutter,0)
+	return
+
+    def hallArrayMake(self):
+	hallArray = []
+	for i in range(25,33):
+	    value = int(self.waiting.getVal(c_int(i)))
+	    pair = [i,value]
+	    hallArray.append(pair)
+	return hallArray
+
+    def close(self):
+        print "sample"
+        self.shutter.closeConnection(1,2)
+        print "\nend sample"
+        return
+
+if __name__ == "__main__":
+	s=shutterControl()
+	s.hallArrayMake()
+	s.sendHome()
+	s.exerciseRoutine()
+	s.changeSense()
+	#s.sendHome()
